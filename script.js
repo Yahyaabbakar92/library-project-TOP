df0f1ccaba6b55237586d4637723d22d08cca521
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-05-20 15:32:00 +0200

    Added book object constructor

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+// Book constructor
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
+function addBookToLibrary() {
+  let userInput = prompt("Enter book name");
+  return userInput;
+}
