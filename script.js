7c4ae63147b4c9ba3462363ecc7b748235bfe6f3
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-05-21 14:22:16 +0200

    Added the right functionality to store data from form and add it to array

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 const myLibrary = [];
 
-// Book constructor
+// The constructor function
 class Book {
   constructor(title, author, pages, read) {
-    this.title = document.querySelector("#title").value;
-    this.author = document.querySelector("#author").value;
-    this.pages = document.querySelector("#pages").value;
-    this.read = document.querySelector("#read").value;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
   }
+
+  // E.g of how a method is written in a constructor
+  // info() {
+  //   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  // }
 }
 
+// The function to add books to the array myLibrary
 const addBookToLibrary = function (e) {
-  e.preventDefault();
+  e.preventDefault(); // Prevent's the form from submitting
 
-  let book = new Book();
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+
+  // The variable that creates a book object
+  let book = new Book(title, author, pages, read);
   console.log(book);
+
+  // The method to move the objects into the array
   myLibrary.push(book);
   console.log(myLibrary);
 };
 
+// The event listener
 document.querySelector(".btn").addEventListener("click", addBookToLibrary);
