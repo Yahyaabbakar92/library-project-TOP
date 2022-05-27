fac6532a02829027ef8f3cd90cc74f520eb80b5f
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-05-27 15:45:51 +0200

    Added functionality for toggle button to appear in a new column

--- script.js
+++ script.js
@@ -39,20 +39,24 @@ const addBookToLibrary = function (e) {
 const displayBooksToTable = function () {
   let tableRow = document.createElement("tr");
   let deleteButton = document.createElement("button");
-  // let editButton = document.createElement("button");
+  let deleteCell = document.createElement("td");
+  let toggleButton = document.createElement("button");
+  let toggleCell = document.createElement("td");
 
   deleteButton.className = "delete";
   deleteButton.innerText = "Delete";
-  // editButton.className = "toggle";
-  // editButton.innerText = "Edit";
+  toggleButton.className = "toggle";
+  toggleButton.innerText = "Toggle";
   myLibrary.forEach((newBook, index) => {
     tableRow.innerHTML = `<td>${newBook.title}</td>
                           <td>${newBook.author}</td>
                           <td>${newBook.pages}</td>
                           <td>${newBook.read}</td>`;
     tableRow.setAttribute("data-book", index);
-    tableRow.appendChild(deleteButton);
-    // tableRow.appendChild(editButton);
+    tableRow.appendChild(deleteCell);
+    deleteCell.appendChild(deleteButton);
+    tableRow.appendChild(toggleCell);
+    toggleCell.appendChild(toggleButton);
   });
 
   tableBody.appendChild(tableRow);
@@ -63,10 +67,12 @@ const displayBooksToTable = function () {
 
 const removeBookFromLibrary = function (e) {
   if (e.target.classList.contains("delete")) {
-    e.target.parentElement.remove();
+    // e.target.parentElement.parentElement.remove();
+    console.log(e.target.parentElement.parentElement.dataset);
   }
   // The function to remove the book from the array
-  myLibrary.splice(e.target.parentElement.dataset, 1);
+  // myLibrary.splice(e.target.parentElement.parentElement.dataset, 1);
+  console.log(myLibrary);
 };
 
 // The event listener to add and display a book to the table
