a027e2c77c3d0119001ebaf9c84b8598c0375bd4
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-08-05 18:22:24 +0200

    Added function to take input and store into array

--- script.js
+++ script.js
@@ -10,24 +10,34 @@ function Book(title, author, pages, read) {
   };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", "376", "Read");
-const harryPotter = new Book(
-  "The philosopher's stone",
-  "J.K. Rowling",
-  "657",
-  "Not read"
-);
-const atomicHabits = new Book("Atomic Habits", "James Clear", "128", "Read");
+// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", "376", "Read");
+// const harryPotter = new Book(
+//   "The philosopher's stone",
+//   "J.K. Rowling",
+//   "657",
+//   "Not read"
+// );
+// const atomicHabits = new Book("Atomic Habits", "James Clear", "128", "Read");
 
-function addBookToLibrary() {
-  myLibrary.push(theHobbit, harryPotter, atomicHabits);
+function addBookToLibrary(e) {
+  e.preventDefault();
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
+
+  let newBook = new Book(title, author, pages, read);
+
+  myLibrary.push(newBook);
+  console.log(myLibrary);
 }
-addBookToLibrary();
 
 function displayBook() {
   myLibrary.forEach((book) => {
     console.log(book);
+    addBookToLibrary();
   });
 }
 
-displayBook();
+let button = document.getElementById("btn");
+button.addEventListener("click", addBookToLibrary);
