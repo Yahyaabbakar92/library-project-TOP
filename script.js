df3f12dce98c8c3d905e76addd50a9b599b6662b
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-08-14 17:10:30 +0200

    Wrote the function to add a row to the table

--- script.js
+++ script.js
@@ -10,16 +10,27 @@ function Book(title, author, pages, read) {
   };
 }
 
-// const theHobbit = new Book("The Hobbit", "J.R.R. Tolkein", "376", "Read");
-// const harryPotter = new Book(
-//   "The philosopher's stone",
-//   "J.K. Rowling",
-//   "657",
-//   "Not read"
-// );
-// const atomicHabits = new Book("Atomic Habits", "James Clear", "128", "Read");
+function addBookToTable() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
+
+  let tbody = document.querySelector("tbody");
+  let tr = document.createElement("tr");
+  let bookArray = [title, author, pages, read];
+
+  bookArray.forEach((book) => {
+    let td = document.createElement("td");
+    td.innerText = book;
+    tr.appendChild(td);
+  });
+
+  tbody.appendChild(tr);
+}
 
 function addBookToLibrary(e) {
+  // The preventDefault() method here cancels the form from being submitted
   e.preventDefault();
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
@@ -30,14 +41,8 @@ function addBookToLibrary(e) {
 
   myLibrary.push(newBook);
   console.log(myLibrary);
+  addBookToTable();
 }
 
-function displayBook() {
-  myLibrary.forEach((book) => {
-    console.log(book);
-    addBookToLibrary();
-  });
-}
-
-let button = document.getElementById("btn");
-button.addEventListener("click", addBookToLibrary);
+let addBookButton = document.getElementById("btn");
+addBookButton.addEventListener("click", addBookToLibrary);
