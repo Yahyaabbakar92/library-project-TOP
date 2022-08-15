75f565d5e9e9e56912ebc8caf26bd36c364a7c5c
Yahya Abbakar <yahyaabbakar@gmail.com>
2022-08-15 17:16:27 +0200

    Added comments and function to remove row from table

--- script.js
+++ script.js
@@ -10,37 +10,60 @@ function Book(title, author, pages, read) {
   };
 }
 
+// The function that removes a row from the table
+function removeRow(e) {
+  let table = document.getElementById("libraryTable");
+  table.deleteRow(e.target.parentNode.rowIndex);
+}
+
 function addBookToTable() {
+  // The variables that store input from the form
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
   let read = document.getElementById("read").value;
 
+  // Creating a button element and giving it an id, click event and text
+  let removeButton = document.createElement("button");
+  removeButton.setAttribute("id", "removeBtn");
+  removeButton.innerText = "Remove Book";
+  removeButton.addEventListener("click", removeRow);
+
   let tbody = document.querySelector("tbody");
   let tr = document.createElement("tr");
-  let bookArray = [title, author, pages, read];
+  let bookArray = [title, author, pages, read]; // The array that stores input form data
 
+  // Using the forEach loop to create a table data cell for each input and appending the cells to the created row outside the loop
   bookArray.forEach((book) => {
     let td = document.createElement("td");
     td.innerText = book;
     tr.appendChild(td);
   });
 
+  // Appending the removeButton to the row as well as the row to the table body
+  tr.appendChild(removeButton);
   tbody.appendChild(tr);
+
+  // console.log(myLibrary[0]);
 }
 
 function addBookToLibrary(e) {
   // The preventDefault() method here cancels the form from being submitted
   e.preventDefault();
+
+  // Storing the values of the input form
   let title = document.getElementById("title").value;
   let author = document.getElementById("author").value;
   let pages = document.getElementById("pages").value;
   let read = document.getElementById("read").value;
 
+  // Instantiating a book using the values from the form
   let newBook = new Book(title, author, pages, read);
 
+  // Adding each book object to the myLibrary array
   myLibrary.push(newBook);
-  console.log(myLibrary);
+
+  // Invoking the function that creates a row and adds it to the table
   addBookToTable();
 }
 
